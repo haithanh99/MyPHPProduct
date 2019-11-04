b1739fe07892df669c857eef678ad656da44a297
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-11-04 15:05:00 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,4 +3,3 @@ $conn = pg_connect("host=ec2-54-235-86-101.compute-1.amazonaws.com port=5432 dbn
 user=cajhsvjnfdjhcz password=904383d81834d725e2c5ee06cb6044887fd36ff594218698bc6fbf474aab488f") 
 or die("Connect Unsuccessful");
 ?>
-a
