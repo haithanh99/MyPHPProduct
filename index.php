58a80a1ffa86aaaba6ff16d3ceb435855a8624fd
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-11-06 15:54:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'top.php';
 			<h4>Xe Đạp 3 Bánh Đa Năng Glee Plus Tím</h4>
 			<img src="https://bit.ly/2NlBSb1>" id="images"/>
 			
-			<p class="price"> Our Price: 629.000 VNĐ></p>
+			<p class="price"> Our Price: 1.490.000 VNĐ></p>
 			<button type="button" class="btn btn-success" background-color=#0B9E1C > Details</button>
 		</div>
 </div>
