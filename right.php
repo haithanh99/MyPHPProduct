99a3be17c6ef83bd1a4f07031fc6b9e3a24bd916
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-11-06 15:29:20 +0700

    s

--- right.php
+++ right.php
@@ -1,3 +1,12 @@
+<div class="col-md-8>
+<div class="col-md-3">
+			<h4>Xe Đạp 3 Bánh Đa Năng Glee Plus Tím</h4>
+			<img src="./images/3300233_1.jpg>" alt="Xe Đạp 3 Bánh Đa Năng Glee Plus Tím" id="images"/>
+			<!-- <p class="list-price text-danger"> List Price: <s>629.000 VNĐ</s></p>  -->
+			<p class="price"> Our Price: 629.000 VNĐ></p>
+			<button type="button" class="btn btn-success" > Details</button>
+		</div>
+</div>
 </div><!-- ket thuc dinh dang cho noi dung -->
 				</div> <!-- ket thucphan dinh dang chung cho content va right -->
 				
