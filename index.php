234f3aaba413740989030925f89cd7d7fc972a75
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-11-06 15:34:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'top.php';
 						<div class="col-md-8">
 <div class="col-md-3">
 			<h4>Xe Đạp 3 Bánh Đa Năng Glee Plus Tím</h4>
-			<img src="images/3300233_1.jpg>" alt="Xe Đạp 3 Bánh Đa Năng Glee Plus Tím" id="images"/>
+			<img src="https://bit.ly/2NlBSb1>" alt="Xe Đạp 3 Bánh Đa Năng Glee Plus Tím" id="images"/>
 			<!-- <p class="list-price text-danger"> List Price: <s>629.000 VNĐ</s></p>  -->
 			<p class="price"> Our Price: 629.000 VNĐ></p>
 			<button type="button" class="btn btn-success" > Details</button>
