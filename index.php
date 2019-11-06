579d3c97db6c87940b4810a0e7c955bfa7e8a992
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-11-06 15:33:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,15 @@ include 'top.php';
 ?>
 <h1>Noi dung tieu de</h1>
 					<div id="noidung"> <!-- dinh dang cho noi dung -->
+						<div class="col-md-8">
+<div class="col-md-3">
+			<h4>Xe Đạp 3 Bánh Đa Năng Glee Plus Tím</h4>
+			<img src="./images/3300233_1.jpg>" alt="Xe Đạp 3 Bánh Đa Năng Glee Plus Tím" id="images"/>
+			<!-- <p class="list-price text-danger"> List Price: <s>629.000 VNĐ</s></p>  -->
+			<p class="price"> Our Price: 629.000 VNĐ></p>
+			<button type="button" class="btn btn-success" > Details</button>
+		</div>
+</div>
 
 <?php
 include 'right.php';
