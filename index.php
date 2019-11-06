9ea1ae2a4a36bca418dde7d3667471fbd088a2e4
haithanh99 <55873463+haithanh99@users.noreply.github.com>
2019-11-06 15:36:23 +0700

    d

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 include 'top.php';
 ?>
 <h1>Noi dung tieu de</h1>
+<div class="col-md-8">
 					<div id="noidung"> <!-- dinh dang cho noi dung -->
-						<div class="col-md-8">
+						
 <div class="col-md-3">
 			<h4>Xe Đạp 3 Bánh Đa Năng Glee Plus Tím</h4>
 			<img src="https://bit.ly/2NlBSb1>" alt="Xe Đạp 3 Bánh Đa Năng Glee Plus Tím" id="images"/>
